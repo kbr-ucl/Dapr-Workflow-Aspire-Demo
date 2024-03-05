c34420f74e7956659762040f7fe3c0502544db0d
KBR <kbr@ucl.dk>
2024-03-05 20:39:39 +0100

    dapr components comment out

--- src/DaprWorkflowAspireDemo.AppHost/Program.cs
+++ src/DaprWorkflowAspireDemo.AppHost/Program.cs
@@ -4,33 +4,33 @@ using System.Collections.Immutable;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-https://devblogs.microsoft.com/dotnet/announcing-dotnet-aspire-preview-2/
+// https://devblogs.microsoft.com/dotnet/announcing-dotnet-aspire-preview-2/
 
-//var stateStore = builder.AddDaprComponent(
-//    "statestore",
-//    "state.redis",
-//    new DaprComponentOptions { LocalPath = "..\\components" });
+////var stateStore = builder.AddDaprComponent(
+////    "statestore",
+////    "state.redis",
+////    new DaprComponentOptions { LocalPath = "..\\components" });
 
-//var pubSub = builder.AddDaprComponent(
-//    "pubsub",
-//    "pubsub.rabbitmq",
-//    new DaprComponentOptions { LocalPath = "..\\components" });
+////var pubSub = builder.AddDaprComponent(
+////    "pubsub",
+////    "pubsub.rabbitmq",
+////    new DaprComponentOptions { LocalPath = "..\\components" });
 
-//builder.AddProject<DaprWorkflowAspireDemo_Order_Api>("order-service")
-//    .WithDaprSidecar("order")
-//    .WithReference(stateStore)
-//    .WithReference(pubSub );
+////builder.AddProject<DaprWorkflowAspireDemo_Order_Api>("order-service")
+////    .WithDaprSidecar("order")
+////    .WithReference(stateStore)
+////    .WithReference(pubSub );
 
-builder.AddProject<DaprWorkflowAspireDemo_Order_Api>("order-service").WithDaprSidecar(
-        new DaprSidecarOptions
-        {
-            AppId = "order",
-            ResourcesPaths = ImmutableHashSet.Create("..\\components")
-        }
-    );
+//builder.AddProject<DaprWorkflowAspireDemo_Order_Api>("order-service").WithDaprSidecar(
+//        new DaprSidecarOptions
+//        {
+//            AppId = "order",
+//            ResourcesPaths = ImmutableHashSet.Create("..\\components")
+//        }
+//    );
 
 
-
+builder.AddProject<DaprWorkflowAspireDemo_Order_Api>("order-service").WithDaprSidecar("order");
 builder.AddProject<DaprWorkflowAspireDemo_Payment_Api>("delivery-payment").WithDaprSidecar("payment");
 builder.AddProject<DaprWorkflowAspireDemo_Kitchen_Api>("kitchen-service").WithDaprSidecar("kitchen");
 builder.AddProject<DaprWorkflowAspireDemo_Delivery_Api>("delivery-service").WithDaprSidecar("delivery");
